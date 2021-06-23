74835d9ed1fa45d2837e86a534ed19a9cf65369e
s181034-MoriJellyfish <morijellyfish@gmail.com>
2021-06-23 17:54:10 +0900

    paraket:コメント追加

--- Assets/VitDeck/Validator/Rules/Paraket/ParaketRuleSet.cs
+++ Assets/VitDeck/Validator/Rules/Paraket/ParaketRuleSet.cs
@@ -45,59 +45,60 @@ namespace VitDeck.Validator
             {
 #if VRC_SDK_VRCSDK2
 
+                //A-1 バージョン制限
                 new UnityVersionRule(LocalizedMessage.Get("VketRuleSetBase.UnityVersionRule.Title", "2018.4.20f1"), "2018.4.20f1"),
-
+                //A-2 SDKバージョン制限
                 new VRCSDKVersionRule(LocalizedMessage.Get("VketRuleSetBase.VRCSDKVersionRule.Title"),
                     new VRCSDKVersion("2020.05.06.12.14"),
                     "https://files.vrchat.cloud/sdk/VRCSDK2-2020.09.15.11.25_Public.unitypackage"),
-
+                //F Scene構造 Dyanamic-Static
                 new ExistInSubmitFolderRule(LocalizedMessage.Get("VketRuleSetBase.ExistInSubmitFolderRule.Title"), VketOfficialAssetData.GUIDs, targetFinder),
-
+                //Blacklist
                 new AssetGuidBlacklistRule(LocalizedMessage.Get("VketRuleSetBase.OfficialAssetDontContainRule.Title"), VketOfficialAssetData.GUIDs),
-
+                //B-3 命名規則
                 new AssetNamingRule(LocalizedMessage.Get("VketRuleSetBase.NameOfFileAndFolderRule.Title"), @"[a-zA-Z0-9 _\.\-\(\)]+"),
-
+                //Vket B-3 パスの長さ制限
                 new AssetPathLengthRule(LocalizedMessage.Get("VketRuleSetBase.FilePathLengthLimitRule.Title", 184), 184),
-
+                //B-4 拡張子制限
                 new AssetExtentionBlacklistRule(LocalizedMessage.Get("VketRuleSetBase.MeshFileTypeBlacklistRule.Title"),
                                                 new string[]{".ma", ".mb", "max", "c4d", ".blend"}
                 ),
-
+                //MaterialとTextureの検証
                 new ContainMatOrTexInAssetRule(LocalizedMessage.Get("VketRuleSetBase.ContainMatOrTexInAssetRule.Title")),
-
+                //B-2 Unitypackageのサイズ制限
                 new FolderSizeRule(LocalizedMessage.Get("VketRuleSetBase.FolderSizeRule.Title"), 50 * MegaByte),
-
+                //ブース内の構造
                 new ExhibitStructureRule(LocalizedMessage.Get("VketRuleSetBase.ExhibitStructureRule.Title")),
-
+                //Staticフラグの管理
                 new StaticFlagRule(LocalizedMessage.Get("VketRuleSetBase.StaticFlagsRule.Title")),
-
+                //C-1 ブースの大きさ
                 new BoothBoundsRule(LocalizedMessage.Get("VketRuleSetBase.BoothBoundsRule.Title"),
                     size: new Vector3(4, 5, 4),
                     margin: 0.01f),
-
+                //D-1 Material制限
                 new AssetTypeLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.MaterialLimitRule.Title", 20),
                     typeof(Material),
                     20,
                     VketOfficialAssetData.MaterialGUIDs),
-
+                //D-5 ライトマップ制限
                 new LightmapSizeLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.LightMapsLimitRule.Title", 1, 512),
                     lightmapCountLimit: 1,
                     lightmapResolutionLimit: 512),
-
+                //E-1 Bakedのみ許可
                 new GlobalIlluminationBakedRule(LocalizedMessage.Get("VketRuleSetBase.GlobalIlluminationBakedRule.Title")),
-
+                //G 使用可能Component 後述
                 new UsableComponentListRule(LocalizedMessage.Get("VketRuleSetBase.UsableComponentListRule.Title"),
                     GetComponentReferences(),
                     ignorePrefabGUIDs: VketOfficialAssetData.GUIDs),
-
+                //G SkinnedMeshの規則
                 new SkinnedMeshRendererRule(LocalizedMessage.Get("VketRuleSetBase.SkinnedMeshRendererRule.Title")),
-
+                //MeshRenderer
                 new MeshRendererRule(LocalizedMessage.Get("VketRuleSetBase.MeshRendererRule.Title")),
-
+                //RefrectionProbeの設定 Paraketでは不可
                 new ReflectionProbeRule(LocalizedMessage.Get("VketRuleSetBase.ReflectionProbeRule.Title")),
-
+                //G Trigger制限
                 new VRCTriggerConfigRule(LocalizedMessage.Get("VketRuleSetBase.VRCTriggerConfigRule.Title"),
                             VRCTriggerBroadcastTypesWhitelist,
                             new VRC_Trigger.TriggerType[] {
@@ -111,91 +112,94 @@ namespace VitDeck.Validator
                                 VRC_Trigger.TriggerType.OnPickupUseUp   },
                             VRCTriggerActionWhitelist,
                             VketOfficialAssetData.GUIDs),
-
+                //? MeshCollider禁止
                 new UseMeshColliderRule(LocalizedMessage.Get("VketRuleSetBase.UseMeshColliderRule.Title")),
-
+                //G トリガー個数制限
                 new VRCTriggerCountLimitRule(LocalizedMessage.Get("VketRuleSetBase.VRCTriggerCountLimitRule.Title", 25), 25),
 
+                //G 以下 ライト制限
+                //Directional count (0)
                 new LightCountLimitRule(LocalizedMessage.Get("VketRuleSetBase.DirectionalLightLimitRule.Title"), UnityEngine.LightType.Directional, 0),
-
+                //Point
                 new LightConfigRule(LocalizedMessage.Get("VketRuleSetBase.PointLightConfigRule.Title"), UnityEngine.LightType.Point, new LightConfigRule.LightConfig(
                             new[] { LightmapBakeType.Baked, LightmapBakeType.Realtime },
                             0, 7,
                             0, 10,
                             0, 15)),
-
+                //Spot
                 new LightConfigRule(LocalizedMessage.Get("VketRuleSetBase.SpotLightConfigRule.Title"), UnityEngine.LightType.Spot, new LightConfigRule.LightConfig(
                             new[] { LightmapBakeType.Baked, LightmapBakeType.Realtime },
                             0, 7,
                             0, 10,
                             0, 15)),
-
+                //Area
                 new LightConfigRule(LocalizedMessage.Get("VketRuleSetBase.AreaLightConfigRule.Title"), UnityEngine.LightType.Area, new LightConfigRule.LightConfig(
                             new[] { LightmapBakeType.Baked },
                             0, 30,
                             0, 10,
                             0, 15)),
-
+                //Area count
                 new LightCountLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.AreaLightLimitRule.Title", 3),
                     UnityEngine.LightType.Area,
                     3),
-
+                //AnimationとColliderに関する制限
                 new AnimationMakesMoveCollidersRule(LocalizedMessage.Get("VketRuleSetBase.AnimationMakesMoveCollidersRule.Title")),
-
+                //Animation Material変更制限 Path制限
                 new AnimationClipRule(LocalizedMessage.Get("VketRuleSetBase.AnimationClipRule.Title")),
-
+                //Animation Alwaysの禁止
                 new AnimationComponentRule(LocalizedMessage.Get("VketRuleSetBase.AnimationComponentRule.Title"), officialPrefabsDetector),
-
+                //Rootmotion 使用禁止
                 new AnimatorComponentRule(LocalizedMessage.Get("VketRuleSetBase.AnimatorComponentRule.Title"),
                     new System.Type[]{
                         typeof(VRC_Pickup),
                         typeof(VRC_ObjectSync)
                     },officialPrefabsDetector),
-
+                //G CanvasRenderはWorldSpaceのみ
                 new CanvasRenderModeRule(LocalizedMessage.Get("VketRuleSetBase.CanvasRenderModeRule.Title")),
-
+                //Canvasサイズ確認
                 new CameraComponentRule(LocalizedMessage.Get("VketRuleSetBase.CameraComponentRule.Title"), maxRenderTextureSize: new Vector2(1024, 1024)),
-
+                //CameraComponent制限
                 new CameraComponentMaxCountRule(LocalizedMessage.Get("VketRuleSetBase.CameraComponentMaxCountRule.Title"), limit: 1),
-
+                //ProjectorComponent制限
                 new ProjectorComponentRule(LocalizedMessage.Get("VketRuleSetBase.ProjectorComponentRule.Title")),
-
+                //ProjectorComponent個数制限
                 new ProjectorComponentMaxCountRule(LocalizedMessage.Get("VketRuleSetBase.ProjectorComponentMaxCountRule.Title"), limit: 1),
-
+                //PickupObjectの制限
                 new PickupObjectSyncPrefabRule(LocalizedMessage.Get("VketRuleSetBase.PickupObjectSyncRule.Title"), VketOfficialAssetData.PickupObjectSyncPrefabGUIDs),
-
+                //G AvatarPedestal制限
                 new AvatarPedestalPrefabRule(LocalizedMessage.Get("VketRuleSetBase.AvatarPedestalPrefabRule.Title"), VketOfficialAssetData.AvatarPedestalPrefabGUIDs),
-
+                //G AudioSource制限
                 new AudioSourcePrefabRule(LocalizedMessage.Get("VketRuleSetBase.AudioSourcePrefabRule.Title"),  VketOfficialAssetData.AudioSourcePrefabGUIDs),
-
+                //G RigidBody制限
                 new RigidbodyRule(LocalizedMessage.Get("VketRuleSetBase.RigidbodyRule.Title")),
-
+                //G 椅子の個数制限
                 new PrefabLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.ChairPrefabLimitRule.Title", 1),
                     VketOfficialAssetData.ChairPrefabGUIDs,
                     1),
-
+                //許可されていないprefabの設置制限
                 new PrefabLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.UnusabePrefabRule.Title", 1),
                     VketOfficialAssetData.VRCSDKPrefabGUIDs,
                     0),
-
+                //G Pirckupの制限
                 new PrefabLimitRule(
                     LocalizedMessage.Get("VketRuleSetBase.PickupObjectSyncPrefabLimitRule.Title", 5),
                     VketOfficialAssetData.PickupObjectSyncPrefabGUIDs,
                     5),
-
+                //VideoPlayerの機能制限
                 new VideoPlayerComponentRule(LocalizedMessage.Get("VketRuleSetBase.VideoPlayerComponentRule.Title")),
-
+                //VideoPlayerの個数制限
                 new VideoPlayerComponentMaxCountRule(LocalizedMessage.Get("VketRuleSetBase.VideoPlayerComponentMaxCountRule.Title"), limit: 1),
-
+                //G Animationの個数制限
                 new AnimatorComponentMaxCountRule(LocalizedMessage.Get("VketRuleSetBase.AnimatorComponentMaxCountRule.Title"), limit: 50)
 #endif
             };
         }
 
 #if VRC_SDK_VRCSDK2 || VRC_SDK_VRCSDK3 
+        //ブロードキャストタイプをlocalのみに限定
         protected virtual VRC_EventHandler.VrcBroadcastType[] VRCTriggerBroadcastTypesWhitelist
         {
             get
@@ -204,7 +208,7 @@ namespace VitDeck.Validator
                     VRC_EventHandler.VrcBroadcastType.Local };
             }
         }
-
+        //使用可能Trigger
         protected virtual VRC_EventHandler.VrcEventType[] VRCTriggerActionWhitelist
         {
             get
@@ -227,7 +231,7 @@ namespace VitDeck.Validator
             }
         }
 #endif
-
+        //使用可能Component
         private ComponentReference[] GetComponentReferences()
         {
             return new ComponentReference[] {
